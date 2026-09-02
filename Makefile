547bbe10bb9409eab22d6760ee90d058c324a5dc
alperenocak <yusufalperenocak10@gmail.com>
2026-09-02 15:16:50 +0300

    Add Docker configuration for WordPress and MariaDB with setup scripts

---
+++ Makefile
@@ -0,0 +1,41 @@
+DATA_DIR := /home/yuocak/data
+COMPOSE := docker compose -f srcs/docker-compose.yml
+
+all: setup up
+
+setup:
+	@mkdir -p $(DATA_DIR)/mariadb
+	@mkdir -p $(DATA_DIR)/wordpress
+
+up:
+	$(COMPOSE) up -d --build
+
+down:
+	$(COMPOSE) down
+
+stop:
+	$(COMPOSE) stop
+
+start:
+	$(COMPOSE) start
+
+restart:
+	$(COMPOSE) restart
+
+logs:
+	$(COMPOSE) logs -f
+
+status:
+	$(COMPOSE) ps
+
+clean: down
+	$(COMPOSE) down --rmi all --volumes --remove-orphans
+	@sudo rm -rf $(DATA_DIR)/mariadb/*
+	@sudo rm -rf $(DATA_DIR)/wordpress/*
+
+fclean: clean
+	@docker system prune -af
+
+re: fclean all
+
+.PHONY: all setup up down stop start restart logs status clean fclean re
---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+
+DB_PASS=$(cat /run/secrets/db_password)
+WP_ADMIN_PASS=$(cat /run/secrets/wp_admin_password)
+WP_USER_PASS=$(cat /run/secrets/wp_user_password)
+
+until mysqladmin ping -h mariadb -u"$MYSQL_USER" -p"$DB_PASS" --silent; do
+    echo "MariaDB bekleniyor..."
+    sleep 2
+done
+
+cd /var/www/wordpress
+
+if [ ! -f wp -login.php] then
+    wp core download --allow-root --local=en_US
+
+    wp config create \
+            --dbname="$MYSQL_DATABASE" \
+            --dbuser="$MYSQL_USER" \
+            --dbpass="$DB_PASS" \
+            --dbhost="mariadb" \
+            --allow-root
+
+        wp core install \
+            --url="https://$DOMAIN_NAME" \
+            --title="$WP_TITLE" \
+            --admin_user="$WP_ADMIN_USER" \
+            --admin_password="$WP_ADMIN_PASS" \
+            --admin_email="$WP_ADMIN_EMAIL" \
+            --skip-email \
+            --allow-root
+
+        wp user create \
+            "$WP_USER" "$WP_USER_EMAIL" \
+            --role=author \
+            --user_pass="$WP_USER_PASS" \
+            --allow-root
+    fi
+
+    exec /usr/sbin/php-fpm8.2 -F
